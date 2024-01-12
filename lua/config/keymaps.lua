fee115d90befe8752177b953f850c4321c4bc713
Liken Tan <likendev@gmail.com>
2024-01-13 00:38:53 +0800

    config: remapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,14 @@
 vim.g.mapleader = ' '
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Moving text block up / down
+vim.keymap.set('v', 'J', [[:m '>+1<CR>gv=gv]])
+vim.keymap.set('v', 'K', [[:m '<-2<CR>gv=gv]])
+
+-- Keeping cursor in middle while searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -18,3 +26,17 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- When pasting, do not replace clipboard
 vim.keymap.set('x', 'p', [["_dP]])
+
+-- Delete without copy into register
+vim.keymap.set('n', '<leader>d', [["_d]])
+vim.keymap.set('v', '<leader>d', [["_d]])
+
+-- Remove Q
+vim.keymap.set('n', 'Q', '<nop>')
+
+-- Vim Quickfix
+vim.keymap.set('n', '<C-j>', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+
