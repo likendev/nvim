cc4da8f25f5e0fba653fc60cdf40316bde9310ac
Liken Tan <likendev@gmail.com>
2024-01-22 00:30:45 +0800

    lsp: add Deno lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -56,6 +56,12 @@ return {
 			})
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
+				root_dir = lspconfig.util.root_pattern("package.json"),
+				single_file_support = false,
+			})
+			lspconfig.denols.setup({
+				capabilities = capabilities,
+				root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 			})
 			lspconfig.svelte.setup({
 				capabilities = capabilities,
